64a949ee36c026bb4bb4feb6eda7799493274641
Anderson Albuquerque <andnunes88@gmail.com>
2018-09-25 14:28:33 -0300

    add método delete

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -141,6 +141,15 @@ class User extends Model{
 
     }
 
+    public function delete(){
+
+        $sql = new Sql();
+
+        $sql->query("CALL sp_users_delete(:iduser)", array(
+            ":iduser"=>$this->getiduser()
+        ));
+    }
+
 }
 
 ?>
